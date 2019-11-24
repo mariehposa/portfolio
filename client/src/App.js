29953fee26ec74344706f002f136c30c33adcf75
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-24 15:52:21 +0000

    create footer component with media logos

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Contact from './components/contact/contact'
+import Contact from './components/contact/contact';
+import Footer from './components/footer/footer'
 
 function App() {
   return (
     <div>
       <Contact />
+      <Footer />
     </div>
   );
 }
