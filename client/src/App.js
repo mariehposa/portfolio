f7c16850e945475f0690606a2d812ca264249599
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-25 06:55:12 +0000

    add route to each component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import Contact from './components/contact/contact';
-import Footer from './components/footer/footer'
+import Footer from './components/footer/footer';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div>
-      <Contact />
-      <Footer />
+      <Route exact path="/" component={Header} />
+      <Route path="about" component={About} />
+      <Route path="portfolio" component={Portfolio}/>
+      <Route path="projects" component={Project}/>
+      <Route path="/contact" component={Contact} />
+      <Route path="footer" component={Footer}/>
     </div>
   );
 }
