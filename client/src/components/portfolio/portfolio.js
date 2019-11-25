f65c7ed8aa1025d8f66498026f971da19926eb60
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-25 15:36:53 +0000

    add images and texts to cards

--- client/src/components/portfolio/portfolio.js
+++ client/src/components/portfolio/portfolio.js
@@ -1,27 +1,121 @@
 import React from "react";
 import Flippy, { FrontSide, BackSide } from 'react-flippy';
+import split from './images/splitTheBill.png'
+import chef from './images/chefPortfolio.png'
+import hairCare from './images/hairCare.png'
+import rickMorty from './images/rickAndMorty.png'
+import starwars from './images/starwars.png'
+import essentialism from './images/essentialism.png'
+import github from './images/github.png';
 
 export default function Portfolio() {
   return (
     <div>
-      <Flippy
-        flipOnHover={false} // default false
-        flipOnClick={true} // default false
-        flipDirection="horizontal" // horizontal or vertical
-        ref={r => (this.flippy = r)} // to use toggle method like this.flippy.toggle()
-        // if you pass isFlipped prop component will be controlled component.
-        // and other props, which will go to div
-        style={{ width: "200px", height: "200px" }} /// these are optional style, it is not necessary
-      >
-        <FrontSide
-          style={{
-            backgroundColor: "#41669d"
-          }}
-        >
-          RICK
-        </FrontSide>
-        <BackSide style={{ backgroundColor: "#175852" }}>ROCKS</BackSide>
-      </Flippy>
+        <h3> PORTFOLIO </h3>
+        <div>
+            
+        <Flippy flipOnHover={true} flipDirection="horizontal" style={{ width: "350px", height: "300px" }} >
+            <FrontSide>
+                <img src={chef} alt="chef-portfolio" style={{ width: "320px", height: "280px" }} />
+            </FrontSide>
+            <BackSide style={{ backgroundColor: "#175852" }}>
+                <h4>Chef Portfolio</h4>
+                <p>Html and Css</p>
+                <div>
+                    <a href="">View site</a>
+                    <a href="https://github.com/mariehposa" title="github profile">
+                        <img src={github} alt="github logo"/>
+                        <p>View on github</p>
+                    </a>
+                </div>
+            </BackSide>
+        </Flippy>
+
+        <Flippy flipOnHover={true} flipDirection="horizontal" style={{ width: "350px", height: "300px" }} >
+            <FrontSide>
+                <img src={starwars} alt="starwars" style={{ width: "320px", height: "280px" }} />
+            </FrontSide>
+            <BackSide style={{ backgroundColor: "#175852" }}>
+                <h4>Starwars</h4>
+                <p>React and pure Javascript</p>
+                <div>
+                    <a href="">View site</a>
+                    <a href="https://github.com/mariehposa" title="github profile">
+                        <img src={github} alt="github logo"/>
+                        <p>View on github</p>
+                    </a>
+                </div>
+            </BackSide>
+        </Flippy>
+
+        <Flippy flipOnHover={true} flipDirection="horizontal" style={{ width: "350px", height: "300px" }} >
+            <FrontSide>
+                <img src={rickMorty} alt="Rick and Morty" style={{ width: "320px", height: "280px" }} />
+            </FrontSide>
+            <BackSide style={{ backgroundColor: "#175852" }}>
+                <h4>Rick & Morty</h4>
+                <p>React and pure Javascript</p>
+                <div>
+                    <a href="">View site</a>
+                    <a href="https://github.com/mariehposa" title="github profile">
+                        <img src={github} alt="github logo"/>
+                        <p>View on github</p>
+                    </a>
+                </div>
+            </BackSide>
+        </Flippy>
+
+        <Flippy flipOnHover={true} flipDirection="horizontal" style={{ width: "350px", height: "300px" }} >
+            <FrontSide>
+                <img src={hairCare} alt="Hair Care" style={{ width: "320px", height: "280px" }} />
+            </FrontSide>
+            <BackSide style={{ backgroundColor: "#175852" }}>
+                <h4>Hair Care</h4>
+                <p>React and pure Javascript</p>
+                <div>
+                    <a href="">View site</a>
+                    <a href="https://github.com/mariehposa" title="github profile">
+                        <img src={github} alt="github logo"/>
+                        <p>View on github</p>
+                    </a>
+                </div>
+            </BackSide>
+        </Flippy>
+
+        <Flippy flipOnHover={true} flipDirection="horizontal" style={{ width: "350px", height: "300px" }} >
+            <FrontSide>
+                <img src={split} alt="Split the Bill" style={{ width: "320px", height: "280px" }} />
+            </FrontSide>
+            <BackSide style={{ backgroundColor: "#175852" }}>
+                <h4>Split the Bill</h4>
+                <p>React Redux</p>
+                <div>
+                    <a href="">View site</a>
+                    <a href="https://github.com/mariehposa" title="github profile">
+                        <img src={github} alt="github logo"/>
+                        <p>View on github</p>
+                    </a>
+                </div>
+            </BackSide>
+        </Flippy>
+
+        <Flippy flipOnHover={true} flipDirection="horizontal" style={{ width: "350px", height: "300px" }} >
+            <FrontSide>
+                <img src={essentialism} alt="Essentialism" style={{ width: "320px", height: "280px" }} />
+            </FrontSide>
+            <BackSide style={{ backgroundColor: "#175852" }}>
+                <h4>Essentialism</h4>
+                <p>Node.js and Postgres</p>
+                <div>
+                    <a href="">View site</a>
+                    <a href="https://github.com/mariehposa" title="github profile">
+                        <img src={github} alt="github logo"/>
+                        <p>View on github</p>
+                    </a>
+                </div>
+            </BackSide>
+        </Flippy>
+        </div>
     </div>
   );
 }
