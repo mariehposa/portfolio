4ca12ffa5ff68215224a18a498de7488515dfba7
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-25 07:22:17 +0000

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,16 @@ import Header from './components/header/header';
 import About from './components/about/about';
 import Portfolio from './components/portfolio/portfolio';
 import Project from './components/project/project';
+import Navigation from './components/navigation/navigation'
 
 function App() {
   return (
     <div>
-      <Route exact path="/" component={Header} />
-      <Route path="/about" component={About} />
-      <Route path="/portfolio" component={Portfolio}/>
-      <Route path="/project" component={Project}/>
+      {/* <Route exact path="/" component={Header} /> */}
+      <Navigation />
+      {/* <Route path="/about" component={About} /> */}
+      {/* <Route path="/portfolio" component={Portfolio}/> */}
+      {/* <Route path="/project" component={Project}/> */}
       <Route path="/contact" component={Contact} />
       <Route path="/footer" component={Footer}/>
     </div>
