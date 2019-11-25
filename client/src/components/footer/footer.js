4ac8a367d3f922703808a200da2b79aa2187a462
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-25 12:02:56 +0000

    add logos

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -1,22 +1,27 @@
 import React from 'react';
+import facebook from './logo/001-facebook.png';
+import twitter from './logo/002-twitter.png';
+import linkedin from './logo/003-linkedin.png';
+import gmail from './logo/004-gmail.png';
+import github from './logo/005-github.png';
 
 export default function Footer() {
     return (
         <div>
             <a href="mariamadedeji.work@gmail.com" title="gmail profile">
-                <img src="gmail.png" alt="gmail"/>
+                <img src={gmail} alt="gmail logo"/>
             </a>
             <a href="https://github.com/mariehposa" title="github profile">
-                <img src="github.png" alt="github"/>
+                <img src={github} alt="github logo"/>
             </a>
             <a href="https://linkedin.com/in/adedeji-mariam" title="linkedin profile">
-                <img src="linkedin.png" alt="linkedin"/>
+                <img src={linkedin} alt="linkedin logo"/>
             </a>
             <a href="https://twitter.com/Mariehposah?s=09" title="twitter profile">
-                <img src="twitter.png" alt="twitter"/>
+                <img src={twitter} alt="twitter logo"/>
             </a>
             <a href="https://www.facebook.com/adedeji.mariam.3" title="facebook profile">
-                <img src="facebook.png" alt="facebook"/>
+                <img src={facebook} alt="facebook logo"/>
             </a>
             <p> COPYRIGHT © 2019 MARIAM ADEDEJI </p>
         </div>
