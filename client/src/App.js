dfca26cc4e2b1d630b980d79bfdb328fedc54fbb
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-25 07:12:17 +0000

    import all components

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,19 @@ import { Route } from 'react-router-dom';
 import Contact from './components/contact/contact';
 import Footer from './components/footer/footer';
 import Header from './components/header/header';
+import About from './components/about/about';
+import Portfolio from './components/portfolio/portfolio';
+import Project from './components/project/project';
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={Header} />
-      <Route path="about" component={About} />
-      <Route path="portfolio" component={Portfolio}/>
-      <Route path="projects" component={Project}/>
+      <Route path="/about" component={About} />
+      <Route path="/portfolio" component={Portfolio}/>
+      <Route path="/project" component={Project}/>
       <Route path="/contact" component={Contact} />
-      <Route path="footer" component={Footer}/>
+      <Route path="/footer" component={Footer}/>
     </div>
   );
 }
