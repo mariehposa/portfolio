93dae9f287ae6ccfb7df5fb4cd0478d35d2452e1
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-26 15:24:47 +0000

    add logos and details

--- client/src/components/about/about.js
+++ client/src/components/about/about.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React from "react";
+import fast from "./logo/001-fast.png";
+import responsive from "./logo/002-responsive.png";
+import intuitive from "./logo/004-lightbulb.png";
+import dynamic from "./logo/006-startup.png";
 
-export default function About () {
-    return (
-        <div>
-            
-        </div>
-    );
+export default function About() {
+  return (
+    <div>
+      <div>
+        <img src={fast} alt="fast" />
+        <h3>Fast</h3>
+        <p>Fast load times and lag free interaction, my highest priority.</p>
+      </div>
+      <div>
+        <img src={responsive} alt="responsive" />
+        <h3>Responsive</h3>
+        <p>My layouts will work on any device, big or small.</p>
+      </div>
+      <div>
+        <img src={intuitive} alt="intuitive" />
+        <h3>Intuitive</h3>
+        <p>My layouts will work on any device, big or small.</p>
+      </div>
+      <div>
+        <img src={dynamic} alt="dynamic" />
+        <h3>Dynamic</h3>
+        <p>Websites don't have to be static, I love making pages come to life.</p>
+      </div>
+    </div>
+  );
 }
