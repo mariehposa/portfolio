78f7de59924eda00d3e8ce185f9e51e9157f553b
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-27 15:43:24 +0000

    add background image to header

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -1,14 +1,19 @@
 import React from 'react';
-// import Particles from 'react-particles-js'
+// import Particles from 'react-particles-js';
+import { HeaderStyle } from './styles'
 
 export default function Header () {
     return (
-        <div id={'header'}>
-            <p>Hello, 
-                <span>I'm Mariam Adedeji.</span>
-            </p>
-            <p>I'm a full stack web developer.</p>
-            <div> View my work</div>
-        </div>
+        <HeaderStyle id={'header'}>
+            <div>
+                <p>Hello, 
+                    <span>I'm Mariam Adedeji.</span>
+                </p>
+                <p>I'm a full stack web developer.</p>
+                <div> View my work</div>
+            </div>
+
+            
+        </HeaderStyle>
     );
 }
---
+++ client/src/components/header/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import image from './img/header.jpg'
+
+export const HeaderStyle = styled.div`
+    background-image: url(${image});
+    /* background-repeat: no-repeat; */
+  background-size: cover;
+  height: 40rem;
+`
