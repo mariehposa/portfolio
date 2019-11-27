fc4d88f0c1fd058b700afb94b7f462fb69955b4f
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-27 10:43:11 +0000

    add header image

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -3,7 +3,7 @@ import Particles from 'react-particles-js'
 
 export default function Header () {
     return (
-        <div>
+        <div style={{backgroundImage: "./img/header"}}>
             
         </div>
     );
