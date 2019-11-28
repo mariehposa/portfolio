1d5f0bf2915d752030818254fddd79bc77e4ad02
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-28 18:21:25 +0000

    work in progress

--- client/src/components/portfolio/portfolio.js
+++ client/src/components/portfolio/portfolio.js
@@ -1,170 +1,218 @@
 import React from "react";
-import { Link } from 'react-router-dom';
-import { Route } from 'react-router-dom';
-import Flippy, { FrontSide, BackSide } from 'react-flippy';
-import split from './images/splitTheBill.png'
-import chef from './images/chefPortfolio.png'
-import hairCare from './images/hairCare.png'
-import rickMorty from './images/rickAndMorty.png'
-import starwars from './images/starwars.png'
-import essentialism from './images/essentialism.png'
-import github from './images/github.png';
-import { StyledAll, StyledNav } from './styles';
-import { StyledLink } from "../navigation/styles";
+import { Route } from "react-router-dom";
+import Flippy, { FrontSide, BackSide } from "react-flippy";
+import split from "./images/splitTheBill.png";
+import chef from "./images/chefPortfolio.png";
+import hairCare from "./images/hairCare.png";
+import rickMorty from "./images/rickAndMorty.png";
+import starwars from "./images/starwars.png";
+import essentialism from "./images/essentialism.png";
+import github from "./images/github.png";
+import { StyledAll, StyledNav, StyledDiv, StyledLink } from "./styles";
 
 export default function Portfolio() {
   return (
-    <StyledAll  id={'portfolio'}>
-        <h3> PORTFOLIO </h3>
+    <StyledAll id={"portfolio"}>
+      <StyledDiv>
+        <h2> PORTFOLIO </h2>
         <div></div>
-        <StyledNav>
-            <StyledLink to="/">ALL</StyledLink>
-            <StyledLink to="/portfolio/html">HTML</StyledLink>
-            <StyledLink to="/portfolio/react">REACT</StyledLink>
-            <StyledLink to="/portfolio/redux">REDUX</StyledLink>
-            <StyledLink to="/portfolio/node">NODE</StyledLink>
-        </StyledNav>
-        <div>
-            <Route exact path="/" component={All}  />
-            <Route path="/portfolio/html" component={Html} />
-            <Route path="/portfolio/react" component={EReact} />
-            <Route path="/portfolio/redux" component={Redux} />
-            <Route path="/portfolio/node" component={Node} />     
-        </div>
+      </StyledDiv>
+      <StyledNav>
+        <StyledLink to="/">ALL</StyledLink>
+        <StyledLink to="/portfolio/html">HTML</StyledLink>
+        <StyledLink to="/portfolio/react">REACT</StyledLink>
+        <StyledLink to="/portfolio/redux">REDUX</StyledLink>
+        <StyledLink to="/portfolio/node">NODE</StyledLink>
+      </StyledNav>
+      <div>
+        <Route exact path="/" component={All} />
+        <Route path="/portfolio/html" component={Html} />
+        <Route path="/portfolio/react" component={EReact} />
+        <Route path="/portfolio/redux" component={Redux} />
+        <Route path="/portfolio/node" component={Node} />
+      </div>
     </StyledAll>
   );
 }
 
-function All(){
-    return (
-        <div>
-            <Html /> <EReact/> <Redux/> <Node/>
-        </div>
-    )
+function All() {
+  return (
+    <div>
+      <Html /> <EReact /> <Redux /> <Node />
+    </div>
+  );
 }
 
-export function Html () {
-    return (
-        <div>
-            <Flippy flipOnHover={true} flipDirection="horizontal" style={{ width: "350px", height: "300px" }} >
-                <FrontSide>
-                    <img src={chef} alt="chef-portfolio" style={{ width: "320px", height: "280px" }} />
-                </FrontSide>
-                <BackSide style={{ backgroundColor: "#175852" }}>
-                    <h4>Chef Portfolio</h4>
-                    <p>Html and Css</p>
-                    <div>
-                        <a href="/a">View site</a>
-                        <a href="https://github.com/mariehposa" title="github profile">
-                            <img src={github} alt="github logo"/>
-                            <p>View on github</p>
-                        </a>
-                    </div>
-                </BackSide>
-            </Flippy>
-        </div>
-    );
+export function Html() {
+  return (
+    <div>
+      <Flippy
+        flipOnHover={true}
+        flipDirection="horizontal"
+        style={{ width: "350px", height: "300px" }}
+      >
+        <FrontSide>
+          <img
+            src={chef}
+            alt="chef-portfolio"
+            style={{ width: "320px", height: "280px" }}
+          />
+        </FrontSide>
+        <BackSide style={{ backgroundColor: "#175852" }}>
+          <h4>Chef Portfolio</h4>
+          <p>Html and Css</p>
+          <div>
+            <a href="/a">View site</a>
+            <a href="https://github.com/mariehposa" title="github profile">
+              <img src={github} alt="github logo" />
+              <p>View on github</p>
+            </a>
+          </div>
+        </BackSide>
+      </Flippy>
+    </div>
+  );
 }
 
-export function EReact () {
-    return (
-        <div>
-            <Flippy flipOnHover={true} flipDirection="horizontal" style={{ width: "350px", height: "300px" }} >
-                <FrontSide>
-                    <img src={starwars} alt="starwars" style={{ width: "320px", height: "280px" }} />
-                </FrontSide>
-                <BackSide style={{ backgroundColor: "#175852" }}>
-                    <h4>Starwars</h4>
-                    <p>React and pure Javascript</p>
-                    <div>
-                        <a href="/a">View site</a>
-                        <a href="https://github.com/mariehposa" title="github profile">
-                            <img src={github} alt="github logo"/>
-                            <p>View on github</p>
-                        </a>
-                    </div>
-                </BackSide>
-            </Flippy>
+export function EReact() {
+  return (
+    <div>
+      <Flippy
+        flipOnHover={true}
+        flipDirection="horizontal"
+        style={{ width: "350px", height: "300px" }}
+      >
+        <FrontSide>
+          <img
+            src={starwars}
+            alt="starwars"
+            style={{ width: "320px", height: "280px" }}
+          />
+        </FrontSide>
+        <BackSide style={{ backgroundColor: "#175852" }}>
+          <h4>Starwars</h4>
+          <p>React and pure Javascript</p>
+          <div>
+            <a href="/a">View site</a>
+            <a href="https://github.com/mariehposa" title="github profile">
+              <img src={github} alt="github logo" />
+              <p>View on github</p>
+            </a>
+          </div>
+        </BackSide>
+      </Flippy>
 
-            <Flippy flipOnHover={true} flipDirection="horizontal" style={{ width: "350px", height: "300px" }} >
-                <FrontSide>
-                    <img src={rickMorty} alt="Rick and Morty" style={{ width: "320px", height: "280px" }} />
-                </FrontSide>
-                <BackSide style={{ backgroundColor: "#175852" }}>
-                    <h4>Rick & Morty</h4>
-                    <p>React and pure Javascript</p>
-                    <div>
-                        <a href="/a">View site</a>
-                        <a href="https://github.com/mariehposa" title="github profile">
-                            <img src={github} alt="github logo"/>
-                            <p>View on github</p>
-                        </a>
-                    </div>
-                </BackSide>
-            </Flippy>
+      <Flippy
+        flipOnHover={true}
+        flipDirection="horizontal"
+        style={{ width: "350px", height: "300px" }}
+      >
+        <FrontSide>
+          <img
+            src={rickMorty}
+            alt="Rick and Morty"
+            style={{ width: "320px", height: "280px" }}
+          />
+        </FrontSide>
+        <BackSide style={{ backgroundColor: "#175852" }}>
+          <h4>Rick & Morty</h4>
+          <p>React and pure Javascript</p>
+          <div>
+            <a href="/a">View site</a>
+            <a href="https://github.com/mariehposa" title="github profile">
+              <img src={github} alt="github logo" />
+              <p>View on github</p>
+            </a>
+          </div>
+        </BackSide>
+      </Flippy>
 
-            <Flippy flipOnHover={true} flipDirection="horizontal" style={{ width: "350px", height: "300px" }} >
-                <FrontSide>
-                    <img src={hairCare} alt="Hair Care" style={{ width: "320px", height: "280px" }} />
-                </FrontSide>
-                <BackSide style={{ backgroundColor: "#175852" }}>
-                    <h4>Hair Care</h4>
-                    <p>React and pure Javascript</p>
-                    <div>
-                        <a href="/a">View site</a>
-                        <a href="https://github.com/mariehposa" title="github profile">
-                            <img src={github} alt="github logo"/>
-                            <p>View on github</p>
-                        </a>
-                    </div>
-                </BackSide>
-            </Flippy>
-        </div>
-    );
+      <Flippy
+        flipOnHover={true}
+        flipDirection="horizontal"
+        style={{ width: "350px", height: "300px" }}
+      >
+        <FrontSide>
+          <img
+            src={hairCare}
+            alt="Hair Care"
+            style={{ width: "320px", height: "280px" }}
+          />
+        </FrontSide>
+        <BackSide style={{ backgroundColor: "#175852" }}>
+          <h4>Hair Care</h4>
+          <p>React and pure Javascript</p>
+          <div>
+            <a href="/a">View site</a>
+            <a href="https://github.com/mariehposa" title="github profile">
+              <img src={github} alt="github logo" />
+              <p>View on github</p>
+            </a>
+          </div>
+        </BackSide>
+      </Flippy>
+    </div>
+  );
 }
 
-export function Redux () {
-    return (
-        <div>
-            <Flippy flipOnHover={true} flipDirection="horizontal" style={{ width: "350px", height: "300px" }} >
-                <FrontSide>
-                    <img src={split} alt="Split the Bill" style={{ width: "320px", height: "280px" }} />
-                </FrontSide>
-                <BackSide style={{ backgroundColor: "#175852" }}>
-                    <h4>Split the Bill</h4>
-                    <p>React Redux</p>
-                    <div>
-                        <a href="/a">View site</a>
-                        <a href="https://github.com/mariehposa" title="github profile">
-                            <img src={github} alt="github logo"/>
-                            <p>View on github</p>
-                        </a>
-                    </div>
-                </BackSide>
-            </Flippy>
-        </div>
-    );
+export function Redux() {
+  return (
+    <div>
+      <Flippy
+        flipOnHover={true}
+        flipDirection="horizontal"
+        style={{ width: "350px", height: "300px" }}
+      >
+        <FrontSide>
+          <img
+            src={split}
+            alt="Split the Bill"
+            style={{ width: "320px", height: "280px" }}
+          />
+        </FrontSide>
+        <BackSide style={{ backgroundColor: "#175852" }}>
+          <h4>Split the Bill</h4>
+          <p>React Redux</p>
+          <div>
+            <a href="/a">View site</a>
+            <a href="https://github.com/mariehposa" title="github profile">
+              <img src={github} alt="github logo" />
+              <p>View on github</p>
+            </a>
+          </div>
+        </BackSide>
+      </Flippy>
+    </div>
+  );
 }
 
-export function Node () {
-    return (
-        <div>
-            <Flippy flipOnHover={true} flipDirection="horizontal" style={{ width: "350px", height: "300px" }} >
-                <FrontSide>
-                    <img src={essentialism} alt="Essentialism" style={{ width: "320px", height: "280px" }} />
-                </FrontSide>
-                <BackSide style={{ backgroundColor: "#175852" }}>
-                    <h4>Essentialism</h4>
-                    <p>Node.js and Postgres</p>
-                    <div>
-                        <a href="/a">View site</a>
-                        <a href="https://github.com/mariehposa" title="github profile">
-                            <img src={github} alt="github logo"/>
-                            <p>View on github</p>
-                        </a>
-                    </div>
-                </BackSide>
-            </Flippy>
-        </div>
-    );
+export function Node() {
+  return (
+    <div>
+      <Flippy
+        flipOnHover={true}
+        flipDirection="horizontal"
+        style={{ width: "350px", height: "300px" }}
+      >
+        <FrontSide>
+          <img
+            src={essentialism}
+            alt="Essentialism"
+            style={{ width: "320px", height: "280px" }}
+          />
+        </FrontSide>
+        <BackSide style={{ backgroundColor: "#175852" }}>
+          <h4>Essentialism</h4>
+          <p>Node.js and Postgres</p>
+          <div>
+            <a href="/a">View site</a>
+            <a href="https://github.com/mariehposa" title="github profile">
+              <img src={github} alt="github logo" />
+              <p>View on github</p>
+            </a>
+          </div>
+        </BackSide>
+      </Flippy>
+    </div>
+  );
 }
--- client/src/components/portfolio/styles.js
+++ client/src/components/portfolio/styles.js
@@ -1,31 +1,53 @@
 import styled from "styled-components";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export const StyledAll = styled.div`
-  padding: 0 5rem;
+  padding: 5rem;
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
+  background: #f5f5f5;
+`;
+
+export const StyledDiv = styled.div`
+  line-height: 0.5rem;
+  align-self: center;
+
+  h2 {
+    font-size: 2.5rem;
+    font-family: "Changa", sans-serif;
+    color: #444649;
+  }
+
+  div {
+    width: 5rem;
+    height: 0.2rem;
+    margin-left: 3rem;
+    background: #444649;
+  }
 `;
 
 export const StyledNav = styled.div`
-  width: 100%;
-  background: #1b242f;
-  border-bottom: 3px solid #04c2c9;
+  width: 70%;
+  display: flex;
+  justify-content: space-evenly;
+  margin: 2rem 0;
 `;
 
 export const StyledLink = styled(Link)`
   text-decoration: none;
   margin: auto 0;
-  color: white;
+  color: #444649;
+  padding: 1rem 1.5rem;
+  font-size: 1.1rem;
 
   &:hover {
-    color: #e31b6d;
-    transition: 0.3s ease-out;
+    color: white;
+    background: #e31b6d;
+    transition: 0.1s ease-out;
   }
 
   &.active {
-   color: #e31b6d;
+    color: #e31b6d;
   }
 `;
