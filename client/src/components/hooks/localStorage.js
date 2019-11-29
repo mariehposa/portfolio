79c74c07236519a257b91d49934224f54de45ff4
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-29 16:39:49 +0000

    create function to get and set selected mode

---
+++ client/src/components/hooks/localStorage.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+export function LocalStorage (key, initialValue) {
+    const [ storedValue, setStoredValue ] = useState(() => {
+        const item = window.localStorage.getItem(key);
+        return item ? item : initialValue;
+    })
+
+    const setValue = (value) => {
+        setStoredValue(value);
+        window.LocalStorage.setItem(key, (value));
+    }
+    return [ storedValue, setValue ];
+}
