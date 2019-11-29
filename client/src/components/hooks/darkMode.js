fe82403b2884b0ff930cdb7a4697bf61b1932ac7
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-29 17:04:15 +0000

    create darkmode hooks

---
+++ client/src/components/hooks/darkMode.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { LocalStorage } from './localStorage';
+
+export function DarkMode () {
+    const [ darkValue, setDarkValue ] = LocalStorage('value', false);
+
+    useEffect(() => {
+        if(darkValue) {
+            document.querySelector('body').classList.add('darkMode')
+        } else {
+            document.querySelector('body').classList.remove('darkMode')
+        }
+    }, [darkValue]);
+
+    return [ darkValue, setDarkValue ];
+}
