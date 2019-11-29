2ec96e184d72de49ca92b1bf182da4dd999a90e7
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-29 12:29:33 +0000

    add target value to anchor tags

--- client/src/components/portfolio/projectFlippy.js
+++ client/src/components/portfolio/projectFlippy.js
@@ -35,13 +35,13 @@ export default ({
           <StyledP>{projectStack}</StyledP>
           <div>
             <StyledBack>
-              <a href={websiteLink} title="View Site">
+              <a href={websiteLink} target="_blank" title="View Site">
                 <img src={domain} alt="domain logo" />
                 <p>View Live Site</p>
               </a>
             </StyledBack>
             <StyledBack>
-              <a href={gitHubLink} title="github profile">
+              <a href={gitHubLink} target="_blank" title="github profile">
                 <img src={github} alt="github logo" />
                 <p>View on github</p>
               </a>
