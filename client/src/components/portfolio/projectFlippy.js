9d925748a4af91e9e42af0c64b6eef671043668e
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-29 11:38:25 +0000

    create flippy cards

---
+++ client/src/components/portfolio/projectFlippy.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Flippy, { FrontSide, BackSide } from "react-flippy";
+import {
+  StyledP,
+  StyledBack,
+  StyledBackAll
+} from "./styles";
+import github from "./images/github.png";
+import domain from "./images/domain.png";
+
+export default ({
+  projectImage,
+  imageAlt,
+  projectTitle,
+  projectStack,
+  gitHubLink
+}) => {
+  return (
+    <Flippy
+      flipOnHover={true}
+      flipDirection="horizontal"
+      style={{ width: "350px", height: "300px" }}
+    >
+      <FrontSide>
+        <img
+          src={projectImage}
+          alt={imageAlt}
+          style={{ width: "320px", height: "280px" }}
+        />
+      </FrontSide>
+      <BackSide>
+        <StyledBackAll>
+          <h3>{projectTitle}</h3>
+          <StyledP>{projectStack}</StyledP>
+          <div>
+            <StyledBack>
+              <a href='/a' title="View Site">
+                <img src={domain} alt="domain logo" />
+                <p>View Live Site</p>
+              </a>
+            </StyledBack>
+            <StyledBack>
+              <a href={gitHubLink} title="github profile">
+                <img src={github} alt="github logo" />
+                <p>View on github</p>
+              </a>
+            </StyledBack>
+          </div>
+        </StyledBackAll>
+      </BackSide>
+    </Flippy>
+  );
+};
