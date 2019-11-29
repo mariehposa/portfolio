9c15fa0f7017f2232b07c9e87ef7385fbc9102a6
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-29 09:12:48 +0000

    Commit styles.js file

---
+++ client/src/components/footer/styles.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const StyledAll = styled.div`
+  padding: 5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  background: #1B242F;
+
+  p {
+      color: #8F9AA7;
+      font-family: "Quicksand", sans-serif;
+      font-size: .8rem;
+    opacity: 0.6;
+    margin-top: 3rem;
+
+    span {
+        color: #E31B6D;
+    }
+  }
+`;
+
+export const StyledDiv = styled.div`
+    display: flex;
+`
+
+export const StyledA = styled.a`
+    margin: 0 1rem;
+    background: #262F38;
+    padding: 1rem;
+    text-decoration: none;
+    text-align: center;
+    align-items: center;
+
+    &:hover {
+        background: #04c2c9;
+        padding: .8rem;
+    }
+`
