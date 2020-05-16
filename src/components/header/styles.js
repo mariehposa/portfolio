b306829409b2e357b5e30a805c78b86592dfd995
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-05-16 13:27:46 +0100

    done"

--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const HeaderStyle = styled.div`
   background: rgb(2,0,36);
-  /* background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(107,147,175,1) 100%, rgba(0,212,255,1) 100%);  */
+  background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(107,147,175,1) 100%, rgba(0,212,255,1) 100%); 
   height: 28rem;
   position: relative;
   text-align: center;
