8c4aa7b0d72f6b2125c8d4b2e44cf298a6c4ebbd
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-05-19 22:17:17 +0100

    create custom flippy cards

--- src/components/portfolio/ProjectFlippy.js
+++ src/components/portfolio/ProjectFlippy.js
@@ -53,5 +53,33 @@ export default ({
         </StyledBackAll>
       </StyledBackTotal>
     </StyledFlippy>
+
+    // <div class="flip-card">
+    //   <div class="flip-card-inner">
+    //     <div class="flip-card-front">
+    //       <img src={projectImage} alt={imageAlt} style={{ width: "320px", height: "280px" }} />
+    //     </div>
+    //     <div class="flip-card-back">
+    //       <StyledBackAll>
+    //         <h3 style={{ color: "#000", }}>{projectTitle}</h3>
+    //         <StyledP>{projectStack}</StyledP>
+    //         <div>
+    //           <StyledBack>
+    //             <a href={websiteLink} target="_blank" rel="noopener noreferrer" title="View Site">
+    //               <img src={domain} alt="domain logo" />
+    //               <p>View Live Site</p>
+    //             </a>
+    //           </StyledBack>
+    //           <StyledBack>
+    //             <a href={gitHubLink} target="_blank" rel="noopener noreferrer" title="github profile">
+    //               <img src={github} alt="github logo" />
+    //               <p>View on github</p>
+    //             </a>
+    //           </StyledBack>
+    //         </div>
+    //       </StyledBackAll>
+    //     </div>
+    //   </div>
+    // </div>
   );
-};
+}; 
