a968c651193a87a5efa31a5bcb0f568aff13f146
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-08-10 18:24:58 +0100

    wip

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,29 +1,19 @@
 import React from "react";
-import fast from "./img/001-fast.png";
-import responsive from "./img/002-responsive.png";
-import intuitive from "./img/004-lightbulb.png";
-import dynamic from "./img/006-startup.png";
 import owner from "./img/owner.jpg";
 import "../../App.css";
 import Zoom from "react-reveal/Zoom";
 import {
   StyledAll,
   StyledDiv,
-  StyledProps,
   AboutMe,
   StyledMe,
-  StyledChart,
-  StyledText,
-  HexaTop,
-  HexaMid,
-  HexaBottom,
 } from "./styles";
 
 export default function About() {
   return (
     <StyledAll id={"about"}>
       <StyledDiv>
-        <h2>ABOUT</h2>
+        <h2>ABOUT ME</h2>
         <div></div>
       </StyledDiv>
 
