d5477450f591aa10924155487483c4818dfc96df
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-08-10 18:23:49 +0100

    wip

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -27,56 +27,6 @@ export default function About() {
         <div></div>
       </StyledDiv>
 
-      <Zoom top cascade>
-        <StyledProps>
-          <div>
-            <div>
-              <HexaTop />
-              <HexaMid>
-                <img src={fast} alt="fast" />
-              </HexaMid>
-              <HexaBottom />
-              <StyledText>Fast</StyledText>
-              <p>
-                Fast load times and lag free interaction.
-              </p>
-            </div>
-            <div>
-              <HexaTop />
-              <HexaMid>
-                <img src={responsive} alt="responsive" />
-              </HexaMid>
-              <HexaBottom />
-              <StyledText>Responsive</StyledText>
-              <p>My layouts will work on any device, big or small.</p>
-            </div>
-          </div>
-          <div>
-            <div>
-              <HexaTop />
-              <HexaMid>
-                <img src={intuitive} alt="intuitive" />
-              </HexaMid>
-              <HexaBottom />
-              <StyledText>Intuitive</StyledText>
-              <p>Strong preference for easy to use, intuitive UX/UI.</p>
-            </div>
-            <div>
-              <HexaTop />
-              <HexaMid>
-                <img src={dynamic} alt="dynamic" />
-              </HexaMid>
-              <HexaBottom />
-              <StyledText>Dynamic</StyledText>
-              <p>
-                Websites don't have to be static, I love making pages come to
-                life.
-              </p>
-            </div>
-          </div>
-        </StyledProps>
-      </Zoom>
-
       <Zoom>
         <AboutMe>
           <StyledMe>
