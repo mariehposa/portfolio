49cea706f39f59d1ead2c3b672754f4b496dff95
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-08-12 20:07:30 +0100

    wip

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -4,11 +4,13 @@ import css from "./img/css.png"
 import html from "./img/html.png"
 import git from "./img/git.png"
 import javascript from "./img/javascript.png"
-import liquid from "./img/liquid.png"
+import node from "./img/node.png"
 import postgres from "./img/postgres.png"
 import python from "./img/python.png"
 import react from "./img/react.png"
-import sqlite from "react-reveal/sqlite";
+import sqlite from "./img/sqlite.png"
+import shopify from "./img/shopify.png"
+import Zoom from "react-reveal/Zoom";
 import {
   StyledAll,
   StyledDiv,
@@ -28,43 +30,43 @@ export default function About() {
           <div>
             <div>
                 <p>Html</p>
-                <img src={} />
+                <img src={html} />
             </div>
             <div>
                 <p>Css</p>
-                <img src={} />
+                <img src={css} />
             </div>
             <div>
                 <p>Javascript</p>
-                <img src={} />
+                <img src={javascript} />
             </div>
             <div>
                 <p>React</p>
-                <img src={} />
+                <img src={react} />
             </div>
             <div>
                 <p>Node</p>
-                <img src={} />
+                <img src={node} />
             </div>
             <div>
                 <p>Postgres</p>
-                <img src={} />
+                <img src={postgres} />
             </div>
             <div>
                 <p>Sqlite3</p>
-                <img src={} />
+                <img src={sqlite} />
             </div>
             <div>
                 <p>Python</p>
-                <img src={} />
+                <img src={python} />
             </div>
             <div>
-                <p>Liquid</p>
-                <img src={} />
+                <p>Shopify</p>
+                <img src={shopify} />
             </div>
             <div>
                 <p>Git</p>
-                <img src={} />
+                <img src={git} />
             </div>
           </div>
         </StyledProps>
