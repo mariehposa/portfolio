48be5ad969593741d247451e7183b3428b1df659
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-08-12 23:12:11 +0100

    add alt: mvp done

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -30,43 +30,43 @@ export default function About() {
         <StyledTotalSkills>
             <StyledSkill>
                 <p>Html</p>
-                <img src={html} />
+                <img src={html} alt="stack-icon" />
             </StyledSkill>
             <StyledSkill>
                 <p>Css</p>
-                <img src={css} />
+                <img src={css} alt="stack-icon" />
             </StyledSkill>
             <StyledSkill>
                 <p>Javascript</p>
-                <img src={javascript} />
+                <img src={javascript} alt="stack-icon" />
             </StyledSkill>
             <StyledSkill>
                 <p>React</p>
-                <img src={react} />
+                <img src={react} alt="stack-icon" />
             </StyledSkill>
             <StyledSkill>
                 <p>Node</p>
-                <img src={node} />
+                <img src={node} alt="stack-icon" />
             </StyledSkill>
             <StyledSkill>
                 <p>Postgres</p>
-                <img src={postgres} />
+                <img src={postgres} alt="stack-icon" />
             </StyledSkill>
             <StyledSkill>
                 <p>Sqlite3</p>
-                <img src={sqlite} />
+                <img src={sqlite} alt="stack-icon" />
             </StyledSkill>
             <StyledSkill>
                 <p>Python</p>
-                <img src={python} />
+                <img src={python} alt="stack-icon" />
             </StyledSkill>
             <StyledSkill>
                 <p>Shopify</p>
-                <img src={shopify} />
+                <img src={shopify} alt="stack-icon" />
             </StyledSkill>
             <StyledSkill>
                 <p>Git</p>
-                <img src={git} />
+                <img src={git} alt="stack-icon" />
             </StyledSkill>
         </StyledTotalSkills>
       </Zoom>
