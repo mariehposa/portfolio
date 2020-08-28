0cd960bf28a06534ebf786af9de3c063dd6948e7
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-08-28 23:04:58 +0100

    wip

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -10,6 +10,11 @@ import python from "./img/python.png"
 import react from "./img/react.png"
 import sqlite from "./img/sqlite.png"
 import shopify from "./img/shopify.png"
+import redux from "./img/redux.jpg"
+import styled from "./img/styled.png"
+import sass from "./img/sass.png"
+import less from "./img/less.jpg"
+import vue from "./img/vue.png"
 import Zoom from "react-reveal/Zoom";
 import {
   StyledAll,
@@ -30,43 +35,63 @@ export default function About() {
         <StyledTotalSkills>
             <StyledSkill>
                 <p>Html</p>
-                <img src={html} alt="stack-icon" />
+                <img src={html} alt="stack-icon" className="s-icon" />
             </StyledSkill>
             <StyledSkill>
                 <p>Css</p>
-                <img src={css} alt="stack-icon" />
+                <img src={css} alt="stack-icon" className="s-icon" />
             </StyledSkill>
             <StyledSkill>
                 <p>Javascript</p>
-                <img src={javascript} alt="stack-icon" />
+                <img src={javascript} alt="stack-icon" className="s-icon" />
+            </StyledSkill>
+            <StyledSkill>
+                <p>Styled components</p>
+                <img src={styled} alt="stack-icon" className="s-icon" />
+            </StyledSkill>
+            <StyledSkill>
+                <p>Less</p>
+                <img src={less} alt="stack-icon" className="s-icon" />
+            </StyledSkill>
+            <StyledSkill>
+                <p>Sass</p>
+                <img src={sass} alt="stack-icon" className="s-icon" />
             </StyledSkill>
             <StyledSkill>
                 <p>React</p>
-                <img src={react} alt="stack-icon" />
+                <img src={react} alt="stack-icon" className="s-icon" />
+            </StyledSkill>
+            <StyledSkill>
+                <p>Redux</p>
+                <img src={redux} alt="stack-icon" className="s-icon" />
+            </StyledSkill>
+            <StyledSkill>
+                <p>Vue</p>
+                <img src={vue} alt="stack-icon" className="s-icon" />
             </StyledSkill>
             <StyledSkill>
                 <p>Node</p>
-                <img src={node} alt="stack-icon" />
+                <img src={node} alt="stack-icon" className="s-icon" />
             </StyledSkill>
             <StyledSkill>
                 <p>Postgres</p>
-                <img src={postgres} alt="stack-icon" />
+                <img src={postgres} alt="stack-icon" className="s-icon" />
             </StyledSkill>
             <StyledSkill>
                 <p>Sqlite3</p>
-                <img src={sqlite} alt="stack-icon" />
+                <img src={sqlite} alt="stack-icon" className="s-icon" />
             </StyledSkill>
             <StyledSkill>
                 <p>Python</p>
-                <img src={python} alt="stack-icon" />
+                <img src={python} alt="stack-icon" className="s-icon" />
             </StyledSkill>
             <StyledSkill>
                 <p>Shopify</p>
-                <img src={shopify} alt="stack-icon" />
+                <img src={shopify} alt="stack-icon" className="s-icon" />
             </StyledSkill>
             <StyledSkill>
                 <p>Git</p>
-                <img src={git} alt="stack-icon" />
+                <img src={git} alt="stack-icon" className="s-icon" />
             </StyledSkill>
         </StyledTotalSkills>
       </Zoom>
