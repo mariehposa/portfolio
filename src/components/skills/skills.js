3c3afcc7d68a0a0e0a54d7e63c4f7aa3de01b4f8
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-08-28 23:39:19 +0100

    change images

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -6,7 +6,7 @@ import git from "./img/git.png"
 import javascript from "./img/javascript.png"
 import node from "./img/node.png"
 import postgres from "./img/postgres.png"
-import python from "./img/python.png"
+import python from "./img/python.jpg"
 import react from "./img/react.png"
 import sqlite from "./img/sqlite.png"
 import shopify from "./img/shopify.png"
@@ -35,15 +35,15 @@ export default function About() {
         <StyledTotalSkills>
             <StyledSkill>
                 <p>Html</p>
-                <img src={html} alt="stack-icon" />
+                <img src={html} alt="stack-icon" className="s-icon" />
             </StyledSkill>
             <StyledSkill>
                 <p>Css</p>
-                <img src={css} alt="stack-icon" />
+                <img src={css} alt="stack-icon" className="s-icon" />
             </StyledSkill>
             <StyledSkill>
                 <p>Javascript</p>
-                <img src={javascript} alt="stack-icon" />
+                <img src={javascript} alt="stack-icon" className="s-icon" />
             </StyledSkill>
             <StyledSkill>
                 <p>Styled components</p>
@@ -75,23 +75,23 @@ export default function About() {
             </StyledSkill>
             <StyledSkill>
                 <p>Postgres</p>
-                <img src={postgres} alt="stack-icon" />
+                <img src={postgres} alt="stack-icon" className="s-icon" />
             </StyledSkill>
             <StyledSkill>
                 <p>Sqlite3</p>
-                <img src={sqlite} alt="stack-icon" />
+                <img src={sqlite} alt="stack-icon" className="s-icon" />
             </StyledSkill>
             <StyledSkill>
                 <p>Python</p>
-                <img src={python} alt="stack-icon" />
+                <img src={python} alt="stack-icon" className="s-icon" />
             </StyledSkill>
             <StyledSkill>
                 <p>Shopify</p>
-                <img src={shopify} alt="stack-icon" />
+                <img src={shopify} alt="stack-icon" className="s-icon" />
             </StyledSkill>
             <StyledSkill>
                 <p>Git</p>
-                <img src={git} alt="stack-icon" />
+                <img src={git} alt="stack-icon" className="s-icon" />
             </StyledSkill>
         </StyledTotalSkills>
       </Zoom>
