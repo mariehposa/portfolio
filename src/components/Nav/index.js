7161beca60ab8056b8060c3b283966673f62aa59
mariehposa <mariamadedeji.work@gmail.com>
2023-07-20 18:52:59 +0100

    add nav

---
+++ src/components/Nav/index.js
@@ -0,0 +1,12 @@
+import logo from "../../assets/logo.png";
+import "./style.scss";
+
+const Nav = () => {
+	return (
+		<a href="https://mariamadedeji.com" className="nav">
+			<img src={logo} alt="logo" />
+		</a>
+	);
+};
+
+export default Nav;
