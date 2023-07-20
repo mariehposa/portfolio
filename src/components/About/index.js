23055bc1cb39a3f81a5f9846bab8b5627ad9bc93
mariehposa <mariamadedeji.work@gmail.com>
2023-07-20 18:52:43 +0100

    add about

---
+++ src/components/About/index.js
@@ -0,0 +1,21 @@
+import "./style.scss";
+
+const About = () => {
+  return (
+    <div className="about">
+      <p className="sub">Hey there, my name is</p>
+      <p className="main">Mariam Adedeji.</p>
+      <p className="summary">I create solutions for the web.</p>
+      <p className="intro">
+        I'm a passionate and accomplished software engineer based in the city of
+        London. I have over four years of experience in full-stack development,
+        encompassing frontend, backend, and cloud engineering. Armed with a BSc
+        in Computer Engineering and an MSc in Computer Science, I thrive on
+        creating innovative solutions at the intersection of technology and
+        great user experience.
+      </p>
+    </div>
+  );
+};
+
+export default About;
